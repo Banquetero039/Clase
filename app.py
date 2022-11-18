6e2a7286466ca754e090c3a80bc648a18cbb88b8
Banquetero039 <117685595+Banquetero039@users.noreply.github.com>
2022-11-18 14:26:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os, time
-import plotly.express as px
 
 opciones = st.sidebar.radio("Filtro",["Inicio","Graficas","Soluciones"])
 
